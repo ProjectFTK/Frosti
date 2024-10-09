8af6d34f3b3a928417776a3da0e842eac26a4314
ProjectFTK <founders@projectftk.com>
2024-10-09 17:35:18 -0400

    point to prod url http

--- src/http.ts
+++ src/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
- const baseUrl = 'https://localhost:7066/';
-// const baseUrl = 'https://api.tryfrosti.com/';
+// const baseUrl = 'https://localhost:7066/';
+ const baseUrl = 'https://api.tryfrosti.com/';
 export default axios.create({
   baseURL: baseUrl,
   headers: {
