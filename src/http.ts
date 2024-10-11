e02306911c26af68ed7e7eed7f8274e76a1a0574
ProjectFTK <founders@projectftk.com>
2024-10-11 16:58:08 -0400

    fix api per usual

--- src/http.ts
+++ src/http.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import https from 'https';
 
- const baseUrl = 'https://localhost:7066/';
-// const baseUrl = 'https://api.tryfrosti.com/';
+// const baseUrl = 'https://localhost:7066/';
+ const baseUrl = 'https://api.tryfrosti.com/';
 export default axios.create({
   baseURL: baseUrl,
   headers: {
